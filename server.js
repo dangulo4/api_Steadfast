0d596d472f91732ab7ce6911c6578ac72cc697d2
Daniel Angulo <dangulo4@gmail.com>
2021-09-29 16:37:03 -0700

    server.js

--- server.js
+++ server.js
@@ -1,34 +1,35 @@
-const express = require('express');
-const cors = require('cors');
-const connectDB = require('./config/db');
-const path = require('path');
+const express = require('express')
+const cors = require('cors')
+const connectDB = require('./config/db')
+const path = require('path')
 
-const app = express();
+const app = express()
 
 // Connect Database
-connectDB();
+connectDB()
 
 // Init Middleware
-app.use(cors());
-app.use(express.json({ extended: false }));
+app.use(cors())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 
 // Define Routes
-app.use('/api/users', require('./routes/users'));
-app.use('/api/auth', require('./routes/auth'));
-app.use('/api/contacts', require('./routes/contacts'));
+app.use('/api/users', require('./routes/users'))
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/contacts', require('./routes/contacts'))
 
 // Server static assets in production
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use(express.static('client/build'));
+  app.use(express.static('client/build'))
 
   app.get('*', (req, res) =>
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-  );
+  )
 }
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`);
-});
+  console.log(`Server started on port ${PORT}`)
+})
